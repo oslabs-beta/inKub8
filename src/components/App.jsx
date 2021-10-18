61d835c4a4cc9afe7536dd72541e999cc1404d16
kingfly555 <gunnarmarino@gmail.com>
2021-10-18 10:54:46 -0500

    Setup boilerplate

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <h1>Test</h1>
+        )
+    }
+}
+export default App;
