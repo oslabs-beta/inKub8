65756953ea5a46b6df0cc4ec480343157d70903b
Elijah Tang <elijahtang@gmail.com>
2021-11-10 15:13:03 -0800

    Metrics

--- src/components/PodMetrics.jsx
+++ src/components/PodMetrics.jsx
@@ -7,19 +7,19 @@ class PodMetrics extends Component {
         <main>
           <iframe
             src="http://localhost:3000/d-solo/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&refresh=10s&from=1636068554083&to=1636072154083&panelId=5"
-            width="350"
+            width="100%"
             height="200"
             frameBorder="0"
           ></iframe>
           <iframe
             src="http://localhost:3000/d-solo/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&refresh=10s&from=1636068569378&to=1636072169378&panelId=7"
-            width="350"
+            width="100%"
             height="200"
             frameBorder="0"
           ></iframe>
           <iframe
             src="http://localhost:3000/d-solo/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&from=1636388084899&to=1636391684899&panelId=12"
-            width="350"
+            width="100%"
             height="200"
             frameBorder="0"
           ></iframe>
--- src/components/ServiceMetrics.jsx
+++ src/components/ServiceMetrics.jsx
@@ -7,19 +7,19 @@ class ServiceMetrics extends Component {
         <main>
           <iframe
             src="http://localhost:3000/d-solo/GzFw-UK7k/prometheus-overview?orgId=1&refresh=60s&from=1636390951732&to=1636394551732&panelId=2"
-            width="350"
+            width="100%"
             height="200"
             frameBorder="0"
           ></iframe>
           <iframe
             src="http://localhost:3000/d-solo/GzFw-UK7k/prometheus-overview?orgId=1&refresh=60s&from=1636390991235&to=1636394591235&panelId=10"
-            width="350"
+            width="100%"
             height="200"
             frameBorder="0"
           ></iframe>
           <iframe
             src="http://localhost:3000/d-solo/GzFw-UK7k/prometheus-overview?orgId=1&refresh=60s&from=1636391001900&to=1636394601900&panelId=4"
-            width="350"
+            width="100%"
             height="200"
             frameBorder="0"
           ></iframe>
