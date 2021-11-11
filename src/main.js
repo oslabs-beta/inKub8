889466ef8019beb7efe04173f6cdcc5d589a41b4
kingfly555 <gunnarmarino@gmail.com>
2021-11-11 10:59:56 -0600

    Forgot to uncomment pty require statement

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const path = require("path");
 const {compileData} = require("./clusterMapping/retrieveData.js");
 //const isDev = require("electron-is-dev");
 const os = require("os");
-//const pty = require("node-pty");
+const pty = require("node-pty");
 
 // if someone is on windows utilize powershell and if not do bash
 const shell = os.platform() === "win32" ? "powershell.exe" : "bash";
