403226a58c2980fe4d2c43d8e5ad9aa2886a83d0
klintonkuach <clintonquach@gmail.com>
2021-11-11 09:54:18 -0800

    able to npm run package

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -21,6 +21,9 @@ module.exports = {
     }
   }*/
 	target: "electron-main",
+	externals: {
+		"pty": require.resolve("node-pty")
+	}
 	//externals: [nodeExternals()]
 	// plugins: [
 	//   new HtmlWebpackPlugin({
